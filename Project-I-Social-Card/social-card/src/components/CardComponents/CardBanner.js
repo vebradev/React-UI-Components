2de24a427d6e111a528920c69e4547efe2568a09
vebradev <justinas@vebra.dev>
2019-05-28 10:08:31 +0300

    Add Card component.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner({ image, alt }) {
+    return <img classname="cardBanner" src={image} alt={alt} />
+}
+
+export default CardBanner;
