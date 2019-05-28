9ad6d57419fdbe25e20c08f3434a8b794cee4784
vebradev <justinas@vebra.dev>
2019-05-28 15:47:09 +0300

    Add components to App body.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import NumberButton from "./components/ButtonComponents/NumberButton";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import OperatorButton from "./components/ButtonComponents/OperatorButton";
+import "./App.css";
+
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="calculator">
+      <div className="row-1">
+        <CalculatorDisplay />
+      </div>
+      <div className="row-2">
+        <ActionButton text="clear" />
+        <OperatorButton text="÷" buttonStyle="operator" />
+      </div>
+      <div className="row-3">
+        <NumberButton text="7" buttonStyle="number" />
+        <NumberButton text="8" buttonStyle="number" />
+        <NumberButton text="9" buttonStyle="number" />
+        <OperatorButton text="*" buttonStyle="operator" />
+      </div>
+      <div className="row-4">
+        <NumberButton text="4" buttonStyle="number" />
+        <NumberButton text="5" buttonStyle="number" />
+        <NumberButton text="6" buttonStyle="number" />
+        <OperatorButton text="−" buttonStyle="operator" />
+      </div>
+      <div className="row-5">
+        <NumberButton text="1" buttonStyle="number" />
+        <NumberButton text="2" buttonStyle="number" />
+        <NumberButton text="3" buttonStyle="number" />
+        <OperatorButton text="+" buttonStyle="operator" />
+      </div>
+      <div className="row-6">
+        <ActionButton text="0" buttonStyle="number" />
+        <OperatorButton text="=" buttonStyle="operator" />
+      </div>
     </div>
   );
 };
